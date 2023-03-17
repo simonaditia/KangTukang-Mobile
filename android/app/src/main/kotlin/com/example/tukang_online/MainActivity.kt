5a8de585cc63669e18720921a138390f49034e49
simonaditia <smnsitumeang@gmail.com>
2023-03-17 14:12:37 +0700

    First Commit + Add Splash Screen & Login Screen

---
+++ android/app/src/main/kotlin/com/example/tukang_online/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tukang_online
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  var _isVisible = false;
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 50.0),
+                height: deviceHeight * 0.20,
+                child: FittedBox(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/logo.png'),
+                    radius: 120,
+                  ),
+                ),
+              ),
+              Container(
+                height: deviceHeight * 0.65,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: LayoutBuilder(builder: (ctx, constraints) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.05,
+                      ),
+                      Center(
+                        child: Text(
+                          'Silakan masukkan Email dan Kata sandi \nAnda untuk masuk ke aplikasi',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.08,
+                      ),
+                      Container(
+                        height: constraints.maxHeight * 0.12,
+                        decoration: BoxDecoration(
+                          color: Color(0xffB4B4B4).withOpacity(0.4),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Center(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "test@gmail.com",
+                              ),
+                              textInputAction: TextInputAction.next,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.02,
+                      ),
+                      Container(
+                        height: constraints.maxHeight * 0.12,
+                        decoration: BoxDecoration(
+                          color: Color(0xffB4B4B4).withOpacity(0.4),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Center(
+                            child: TextField(
+                              obscureText: _isVisible ? false : true,
+                              decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isVisible = !_isVisible;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    _isVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                border: InputBorder.none,
+                                hintText: "Password",
+                              ),
+                              textInputAction: TextInputAction.done,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Lupa Password?",
+                              style: TextStyle(
+                                color: Color(0xffFF5403),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: constraints.maxHeight * 0.12,
+                        margin: EdgeInsets.only(
+                          top: constraints.maxHeight * 0.05,
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 22),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xffFF5403),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.02,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: "Belum punya akun!",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          children: [
+                            TextSpan(
+                                text: " Register",
+                                style: TextStyle(
+                                  color: Color(0xffFF5403),
+                                  fontSize: 18,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {}),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
